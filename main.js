317dd602ccffacc96135c1dd24d2d1efe80bc921
wey <pukaimakaola@gmail.com>
2024-08-03 14:27:20 -0400

    added javascript for share button

--- main.js
+++ main.js
@@ -1,34 +1,19 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./style.css";
-// import App from "./App";
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
 shareBtn = document.querySelectorAll(".btn");
 profile = document.querySelector(".default");
 desktopShare = document.querySelector(".share-container");
-clicked = document.querySelector(".clicked");
+mobileShare = document.querySelector(".clicked");
 card = document.querySelector(".card");
 
 let visibleBtn = shareBtn[0];
-// console.log(card.scrollWidth);
+
 visibleBtn.addEventListener("click", (e) => {
   if (card.scrollWidth >= 720) {
     console.log("works", desktopShare);
     desktopShare.classList.toggle("hidden");
   } else {
-    if (profile.style !== "none") {
-      profile.style.display = "none";
-      clicked.style.display = "flex";
-      visibleBtn = shareBtn[1];
-    } else {
-      console.log("second click");
-    }
+    mobileShare.classList.toggle("hidden");
+    profile.classList.toggle("hidden");
+    visibleBtn = shareBtn[1];
+    console.log("clicked", visibleBtn);
   }
 });
